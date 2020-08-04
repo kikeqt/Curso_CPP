9fa005673b19b7cb05f844d8712c8ac695bc608f
Enrique Quijano <kike.qt@gmail.com>
2020-08-03 22:59:12 -0500

    Create ejemplo_1_10.cpp

---
+++ ejemplo_1_10.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main ()
+{
+    bool expresion = true;
+
+    if (expresion) {
+        std::cout << "Aquí entro si la expresión se cumple" << std::endl;
+    }
+
+    return EXIT_SUCCESS;
+}
