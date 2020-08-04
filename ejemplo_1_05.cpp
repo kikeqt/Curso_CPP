d475648222930e3061e751187d48df5930102919
Enrique Quijano <kike.qt@gmail.com>
2020-08-03 22:58:45 -0500

    Create ejemplo_1_05.cpp

---
+++ ejemplo_1_05.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << 2.0 + 10 << std::endl; // 12.0
+    std::cout << 2.0 - 10 << std::endl; // -8.0
+    std::cout << 10 / 3 << std::endl; // 3
+    std::cout << 10 % 3 << std::endl; // 1
+    std::cout << 10 * 3 << std::endl; // 30
+
+    return EXIT_SUCCESS;
+}
